8a2c7ccfc32a47c01e4f8ea20063314b32f5069a
Elmera Azadpour <eazadpour@usgs.gov>
2026-03-11 10:36:42 -0700

    renaming targets, condensing tagets, colnames sect

--- gw_ci/1_fetch.R
+++ gw_ci/1_fetch.R
@@ -51,7 +51,7 @@ p1_targets <- list(
         )
       } else {
         p1_date_complete |>
-          dplyr::select(-c(parquet_file, complete)) |>
+          dplyr::select(-c(parquet_file, complete, matches("(mp4)"))) |>
           tidyr::pivot_longer(cols = matches("*_image_file"), 
                               names_to = "remote_image_type",
                               values_to = "remote_image_file_key")
