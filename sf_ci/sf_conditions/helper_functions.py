919028f04526725b061ec9cfdf9ee70494ca42f5
Jeffrey Kwang <jkwang@usgs.gov>
2026-03-11 09:52:20 -0500

    s3 pull integration

--- sf_ci/sf_conditions/helper_functions.py
+++ sf_ci/sf_conditions/helper_functions.py
@@ -1,31 +1,58 @@
 import os
+import re
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 
-def generate_date_list(start_date, end_date):
-    """Generates the list of dates given a start and end date."""
+def generate_image_list(end_date_str, intervals, metadata, column = 'desktop_CONUS_image_file'):
+    """Generates the list of images."""
+    max_delta = timedelta(0)
+    image_lists = []
+    for interval in intervals:
+        # Date to run based on argument to Snakemake
+        # Use snakemake --cores all --config date="YYYY-MM-DD"
+        end_date = datetime.strptime(end_date_str,"%Y-%m-%d").date()
+        start_date = find_start_date(end_date, interval)
+        delta = end_date - start_date
 
-    # Parse input strings into datetime objects
-    start = datetime.strptime(start_date, "%Y-%m-%d")
-    end = datetime.strptime(end_date, "%Y-%m-%d")
+        # Generate list of dates
+        date_list = []
+        current = start_date
+        while current <= end_date:
+            date_list.append(current.strftime("%Y-%m-%d"))
+            current += timedelta(days=1)
 
-    # Generate list of dates
-    date_list = []
-    current = start
-    while current <= end:
-        date_list.append(current.strftime("%Y-%m-%d"))
-        current += timedelta(days=1)
+        image_list = ["figures/sf-" + str(date)+ ".png" for date in date_list]
+        image_lists += [image_list]
 
-    return date_list
+        # this list is from the maxiumum interval
+        if delta > max_delta:
+            max_delta = delta
 
-def strip_date_list(filepath_list, prefix = "sf_categorizations_", suffix = ".parquet"):
-    """Takes a list of parquet files with dates and makes a list of just the dates"""
+            s3_image_list = metadata[metadata['date'].isin(date_list)][column].tolist()
+            parquet_list = metadata[metadata['date'].isin(date_list)]["parquet_file"].tolist()
 
-    date_list = []
-    for filepath in filepath_list:
-        dirname = os.path.dirname(filepath)
+            image_download_list = []
+            image_make_list = []
+            parquet_make_list = []
+            for j, s3_image in enumerate(s3_image_list):
+                if s3_image == 'NA':
+                    parquet_make_list += [parquet_list[j]]
+                    image_make_list += [image_list[j]]
+                else:
+                    image_download_list += [image_list[j]]
 
-        date = filepath[len(dirname) + 1 + len(prefix): - len(suffix)]
-        date_list += [date]
+    return image_lists, image_download_list, image_make_list, parquet_make_list
 
-    return date_list
+def find_start_date(end_date, interval):
+    num, unit = re.match(r"(\d+)([A-Za-z]+)", interval).groups()
+    if unit == "d":
+        return (end_date - relativedelta(days=int(num)))
+    elif unit == "w":
+        return (end_date - relativedelta(weeks=int(num)))
+    elif unit == "m":
+        return (end_date - relativedelta(months=int(num)))
+    elif unit == "y":
+        return (end_date - relativedelta(years=int(num)))
+    else:
+        raise ValueError(f"Invalid interval format")
