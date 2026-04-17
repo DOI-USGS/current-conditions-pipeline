39dc4f5ff74d7fcbe50326eeacd1829dc20f659d
Elmera Azadpour <eazadpour@usgs.gov>
2026-04-17 09:58:06 -0700

    edit p3_states_gw_webps_config pattern map

--- gw_ci/3_visualize.R
+++ gw_ci/3_visualize.R
@@ -404,9 +404,9 @@ p3_targets <- list(
         p1_date_incomplete[["date"]]
       ))
     },
-    pattern = cross(
-      map(p1_date_incomplete),
-      map(p0_states_area_info_df, p3_states_gw_webps)
+    pattern = map(
+      cross(p1_date_incomplete, p0_states_area_info_df),
+      p3_states_gw_webps
     )
   ),
   
