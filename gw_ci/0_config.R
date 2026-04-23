bde0906ad44d7c0f9357a2e80bf4a20c09eb4c79
Elmera Azadpour <eazadpour@usgs.gov>
2026-04-23 06:52:25 -0700

    edit last year interval to pull from jan1 2025

--- gw_ci/0_config.R
+++ gw_ci/0_config.R
@@ -66,7 +66,7 @@ p0_targets <- list(
       "last-month" = base::months(1),
       "last-3-months" = base::months(3),
       "last-6-months" = base::months(6),
-      "last-year" = base::months(12)
+      "last-year" = base::months(15) + lubridate::days(20)
     )
   ),
   tar_target(
