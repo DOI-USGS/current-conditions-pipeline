7d52cc53ab30f56f5eca2c01bf24157a68d38d7b
Zemmels, Joseph R <jzemmels@usgs.gov>
2026-04-24 21:31:11 +0000

    TEMPORARILY comment-out all but last month date ranges

--- gw_ci/0_config.R
+++ gw_ci/0_config.R
@@ -67,10 +67,10 @@ p0_targets <- list(
   tar_target(
     p0_intervals,
     c(
-      "last-month" = base::months(1),
-      "last-3-months" = base::months(3),
-      "last-6-months" = base::months(6),
-      "last-year" = base::months(15) + lubridate::days(21)
+      "last-month" = base::months(1)
+      #,"last-3-months" = base::months(3),
+      #"last-6-months" = base::months(6),
+      #"last-year" = base::months(15) + lubridate::days(21)
     )
   ),
   tar_target(
