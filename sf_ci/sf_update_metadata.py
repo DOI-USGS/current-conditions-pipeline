a1c14a20bec75f0ef8a50f2a74ebb87028d557a9
Jeffrey Kwang <jkwang@usgs.gov>
2026-04-28 07:04:53 -0500

    commited unsaved portion

--- sf_ci/sf_update_metadata.py
+++ sf_ci/sf_update_metadata.py
@@ -13,17 +13,6 @@ SF_PATH = "visualizations/current_conditions/streamflow/"
 METADATA_KEY = "metadata/sf_file_metadata.csv"
 EXPECTED_OUTPUTS = {
     "parquet_file": "data/sf_categorizations_{date}.parquet",
-<<<<<<< HEAD
-    "mobile_CONUS_image_file": "images/sf-mobile-CONUS-{date}.png",
-    "mobile_AK_image_file": "images/sf-mobile-AK-{date}.png",
-    "mobile_HI_image_file": "images/sf-mobile-HI-{date}.png",
-    "mobile_PR_VI_image_file": "images/sf-mobile-PR_VI-{date}.png",
-    "mobile_GU_MP_image_file": "images/sf-mobile-GU_MP-{date}.png",
-    "mobile_AS_image_file": "images/sf-mobile-AS-{date}.png",
-    "desktop_CONUS_OCONUS_image_file": "images/sf-desktop-CONUS_OCONUS-{date}.png",
-    "movie_3d": "videos/sf-movie-{date}-back-3d.mp4",
-    "movie_5d": "videos/sf-movie-{date}-back-5d.mp4",
-=======
     "mobile_CONUS_image_file": "images/sf-mobile-CONUS-{date}.webp",
     "mobile_AK_image_file": "images/sf-mobile-AK-{date}.webp",
     "mobile_HI_image_file": "images/sf-mobile-HI-{date}.webp",
@@ -32,7 +21,6 @@ EXPECTED_OUTPUTS = {
     "mobile_AS_image_file": "images/sf-mobile-AS-{date}.webp",
     "desktop_CONUS_OCONUS_image_file": "images/sf-desktop-CONUS_OCONUS-{date}.webp",
     "desktop_static_CONUS_OCONUS_image_file": "images/sf-desktop-static-CONUS_OCONUS-{date}.png",
->>>>>>> 801b69c (specify webp for metadata, not png)
 }
 
 parser = argparse.ArgumentParser()
