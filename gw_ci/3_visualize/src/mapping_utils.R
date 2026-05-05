f10266e8aca823d3ad8f5f24e5701c139b2b696e
Corson-Dosch <hcorson-dosch@usgs.gov>
2026-05-04 23:13:27 -0500

    adjust scaling for mobile

--- gw_ci/3_visualize/src/mapping_utils.R
+++ gw_ci/3_visualize/src/mapping_utils.R
@@ -413,11 +413,11 @@ plot_gw_image <- function(gw_parquet_file, date, area_name, area_info_df, area_s
   
   # Export dims
   if (image_screen_type == "desktop" && area_name == "CONUS_OCONUS") {
-    export_width <- viz_cfg$width
-    export_height <- viz_cfg$height
+    export_width <- viz_cfg$desktop_conus_oconus_width
+    export_height <- viz_cfg$desktop_height
   } else if (image_screen_type == "desktop" && !(area_name == "CONUS_OCONUS")) {
     export_width <- viz_cfg$desktop_width
-    export_height <- viz_cfg$height
+    export_height <- viz_cfg$desktop_height
   } else if (image_screen_type == "mobile") {
     export_width <- viz_cfg$mobile_width
     export_height <- viz_cfg$mobile_height
@@ -439,7 +439,6 @@ plot_gw_image <- function(gw_parquet_file, date, area_name, area_info_df, area_s
       ),
       function(area_name, area_proj, area_state_list, area_scale_factor,
                extent_info, area_poly_sf) {
-        
         # Build area base plot
         if (draw_base) {
           p <- plot_base(
@@ -498,7 +497,7 @@ plot_gw_image <- function(gw_parquet_file, date, area_name, area_info_df, area_s
       }
     ) |>
       set_names(area_info_df[["name"]])
-
+    
     # arrange plots
     extent_info <- set_names(extent_info, area_info_df[["name"]])
     gw_plot <- generate_landscape_condensed(
@@ -570,37 +569,52 @@ plot_gw_image <- function(gw_parquet_file, date, area_name, area_info_df, area_s
     
     # Build area symbol plot
     if (draw_symbols) {
+      if (image_screen_type == "mobile")
       # Generate appropriate scaling parameters for each area
       # _NOTE: this is a first stab at adjusting these for different areas. I
       # suspect we will also need to make some further adjustments for mobile_
-      scale_cfg <- scale_cfg |>
-        mutate(
-          max_vector_height = max_vector_height_px * reference_scale,
-          mid_vector_height = mid_vector_height_px * reference_scale,
-          min_vector_height = min_vector_height_px * reference_scale,
-          max_vector_width = max_vector_width_px * reference_scale,
-          mid_vector_width = max_vector_width * mid_factor,
-          min_vector_width = max_vector_width * min_factor,
-          normal_width = max_vector_width * min_factor,
-          max_peak_width = max_factor,
-          mid_peak_width = max_factor * mid_factor,
-          min_peak_width = max_factor * min_factor
-        )
-      # if (image_screen_type == "mobile") {
-      #   scale_cfg <- scale_cfg |>
-      #     mutate(
-      #       max_vector_height = max_vector_height * extent_info[["rel_height"]],
-      #       mid_vector_height = mid_vector_height * extent_info[["rel_height"]],
-      #       min_vector_height = min_vector_height * extent_info[["rel_height"]],
-      #       max_vector_width = max_vector_width * extent_info[["rel_width"]],
-      #       mid_vector_width = max_vector_width * mid_factor,
-      #       min_vector_width = max_vector_width * min_factor,
-      #       normal_width = max_vector_width * min_factor,
-      #       max_peak_width = max_factor_mobile,
-      #       mid_peak_width = max_factor_mobile * mid_factor,
-      #       min_peak_width = max_factor_mobile * min_factor
-      #     )
-      # }
+      if (image_screen_type == "desktop") {
+        scale_cfg <- scale_cfg |>
+          mutate(
+            max_vector_height = max_vector_height_px * reference_scale,
+            mid_vector_height = mid_vector_height_px * reference_scale,
+            min_vector_height = min_vector_height_px * reference_scale,
+            max_vector_width = max_vector_width_px * reference_scale,
+            mid_vector_width = max_vector_width * mid_factor,
+            min_vector_width = max_vector_width * min_factor,
+            normal_width = max_vector_width * min_factor,
+            max_peak_width = max_factor,
+            mid_peak_width = max_factor * mid_factor,
+            min_peak_width = max_factor * min_factor
+          )
+      } else if (image_screen_type == "mobile") {
+        scale_cfg <- scale_cfg |>
+          mutate(
+            max_vector_height = mobile_max_vector_height_px * reference_scale,
+            mid_vector_height = mobile_mid_vector_height_px * reference_scale,
+            min_vector_height = mobile_min_vector_height_px * reference_scale,
+            max_vector_width = mobile_max_vector_width_px * reference_scale,
+            mid_vector_width = max_vector_width * mid_factor,
+            min_vector_width = max_vector_width * min_factor,
+            normal_width = max_vector_width * min_factor,
+            max_peak_width = max_factor,
+            mid_peak_width = max_factor * mid_factor,
+            min_peak_width = max_factor * min_factor
+          )
+        # scale_cfg <- scale_cfg |>
+        #   mutate(
+        #     max_vector_height = max_vector_height * extent_info[["rel_height"]],
+        #     mid_vector_height = mid_vector_height * extent_info[["rel_height"]],
+        #     min_vector_height = min_vector_height * extent_info[["rel_height"]],
+        #     max_vector_width = max_vector_width * extent_info[["rel_width"]],
+        #     mid_vector_width = max_vector_width * mid_factor,
+        #     min_vector_width = max_vector_width * min_factor,
+        #     normal_width = max_vector_width * min_factor,
+        #     max_peak_width = max_factor_mobile,
+        #     mid_peak_width = max_factor_mobile * mid_factor,
+        #     min_peak_width = max_factor_mobile * min_factor
+        #   )
+      }
       
       p <- plot_gw(
         gw_parquet_file = gw_parquet_file,
@@ -688,17 +702,6 @@ plot_gw_image <- function(gw_parquet_file, date, area_name, area_info_df, area_s
         )
     }
   }
-  
-  # Export
-  # if (image_screen_type == "desktop") {
-  #   export_width <- viz_cfg$width
-  #   export_height <- viz_cfg$height
-  # } else if (image_screen_type == "mobile") {
-  #   export_width <- viz_cfg$mobile_width
-  #   export_height <- viz_cfg$mobile_height
-  # } else {
-  #   stop(message("image_screen_type must be either 'desktop' or 'mobile'"))
-  # }
 
   bg_col <- if (transparent_bg) "transparent" else viz_cfg$bg_col
 
