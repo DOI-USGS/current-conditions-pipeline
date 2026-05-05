9dfebb9b036e6d556e45871a19ee5cf99b9e7bb1
Jeffrey Kwang <jkwang@usgs.gov>
2026-05-05 12:27:07 -0500

    mask points to geometry

--- sf_ci/sf_conditions/plot/plot_sf.py
+++ sf_ci/sf_conditions/plot/plot_sf.py
@@ -73,6 +73,9 @@ def plot_daily_sf_condition(
     minx, miny, maxx, maxy = reference_gdf.to_crs(layout_params["proj"][0]).total_bounds
     reference_length = max(maxx - minx, maxy - miny) * figure_params["axis_buffer"]
 
+    # filter out markers outside of the reference gdf
+    dv_gdf_day = dv_gdf_day[dv_gdf_day.within(reference_gdf.union_all())]
+
     # Set up figure
     fig = plt.figure(1, figsize=(layout_params["figure_dimensions"]), facecolor='none')
 
