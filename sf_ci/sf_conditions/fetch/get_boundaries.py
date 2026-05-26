0b7c79475637b4c90f01b1793e32dd4fb65226ff
Jeffrey Kwang <jkwang@usgs.gov>
2026-05-26 13:08:18 -0500

    change get_boundaries

--- sf_ci/sf_conditions/fetch/get_boundaries.py
+++ sf_ci/sf_conditions/fetch/get_boundaries.py
@@ -2,82 +2,45 @@ from pygris import states
 from pygris import counties
 import geopandas as gpd
 
-def get_boundaries(
-        resolution,
-        year,
-        conus_geojson,
-        ak_geojson,
-        hi_geojson,
-        prvi_geojson,
-        gump_geojson,
-        as_geojson,
-):
-    """Gets boundary geometry from Census data
+STATES_50_DC = [
+    'AL','AK','AZ','AR','CA','CO','CT','DC','DE','FL','GA','HI','ID','IL','IN',
+    'IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH',
+    'NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT',
+    'VT','VA','WA','WV','WI','WY'
+]
 
-    Parameters
-    ----------
-    resolution: string
-        resolution of the census daa
-    year: integer
-        year of the census data
-    conus_geojson: string
-        filepath and name of .geojson file for conus
-    ak_geojson: string
-        filepath and name of .geojson file for alaska
-    hi_geojson: string
-        filepath and name of .geojson file for hawaii
-    prvi_geojson: string
-        filepath and name of .geojson file for puerto rico and the virgin islands
-    gump_geojson: string
-        filepath and name of .geojson file for the mariana islands
-    as_geojson: string
-        filepath and name of .geojson file for american somoa 
-
-    Returns
-    -------
-        Saved geometries as .geojson files
-
-    """
+def get_boundaries(resolution, year, output):
+    """Gets boundary geometry from Census data and saves as geojson files."""
 
     us = states(cb=True, resolution=resolution, year=year)
 
+    # Regional groupings
     exclude_for_conus = {"AK", "HI", "PR", "VI", "GU", "MP", "AS"}
     conus_gpd = us[~us["STUSPS"].isin(exclude_for_conus)]
-    ak_gpd = us[us["STUSPS"] == "AK"]
-    hi_gpd = us[us["STUSPS"] == "HI"]
     prvi_gpd = us[us["STUSPS"].isin(["PR", "VI"])]
     gump_gpd = us[us["STUSPS"].isin(["GU", "MP"])]
 
-    # exclude swains island and rose island
-    as_cnty = counties(state="AS",cb=True, resolution=resolution, year=year)
+    # American Samoa - exclude Swains Island and Rose Island
+    as_cnty = counties(state="AS", cb=True, resolution=resolution, year=year)
     as_no_swains = as_cnty[~as_cnty["NAME"].isin(["Swains Island", "Rose Island"])]
     as_gpd = gpd.GeoDataFrame({"geometry": [as_no_swains.union_all()]}, crs=us.crs)
 
-    conus_gpd.to_file(conus_geojson)
-    ak_gpd.to_file(ak_geojson)
-    hi_gpd.to_file(hi_geojson)
-    prvi_gpd.to_file(prvi_geojson)
-    gump_gpd.to_file(gump_geojson)
-    as_gpd.to_file(as_geojson)
+    # Save regional files
+    conus_gpd.to_file(output["conus_geojson"])
+    prvi_gpd.to_file(output["prvi_geojson"])
+    gump_gpd.to_file(output["gump_geojson"])
+    as_gpd.to_file(output["as_geojson"])
+
+    # Save individual state/DC files
+    for st in STATES_50_DC:
+        key = st.lower() + "_geojson"
+        if key in output.keys():
+            st_gpd = us[us["STUSPS"] == st]
+            st_gpd.to_file(output[key])
+
 
 if __name__ == "__main__":
     resolution = snakemake.params["resolution"]
     year = snakemake.params["year"]
 
-    conus_geojson = snakemake.output["conus_geojson"]
-    ak_geojson = snakemake.output["ak_geojson"]
-    hi_geojson = snakemake.output["hi_geojson"]
-    prvi_geojson = snakemake.output["prvi_geojson"]
-    gump_geojson = snakemake.output["gump_geojson"]
-    as_geojson = snakemake.output["as_geojson"]
-
-    get_boundaries(
-        resolution,
-        year,
-        conus_geojson,
-        ak_geojson,
-        hi_geojson,
-        prvi_geojson,
-        gump_geojson,
-        as_geojson,
-    )
+    get_boundaries(resolution, year, snakemake.output)
